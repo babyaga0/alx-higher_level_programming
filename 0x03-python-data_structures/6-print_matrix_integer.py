9a9cc737d824f9b5a2824e660d5a436d8a2c38b2
root <you@example.com>
2024-03-18 16:12:52 -0700

    python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for AZ in matrix:
-        for WX in row:
+        for WX in AZ:
             print("{:d}".format(WX), end=" " if WX != AZ[-1] else "")
         print()
