aafd1e25f4f98e43d48aed25b949a45b0e1562a1
root <you@example.com>
2024-04-02 16:19:26 -0700

    d

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def safe_print_division(r, s):
+def safe_print_division(a, b):
     try:
-        tid = r / s
+        div = a / b
     except (TypeError, ZeroDivisionError):
-        tid = None
+        div = None
     finally:
         print("Inside result: {}".format(div))
-    return (tid)
+    return (div)
