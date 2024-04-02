6fc8614abb574cd74ad0e7976252c0c11acb3123
root <you@example.com>
2024-04-02 15:55:57 -0700

    d

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def display_elements(input_list=[], count=0):
+    """Prints a specified number of elements from a list.
+
+    Args:
+        input_list (list): The list from which elements are to be printed.
+        count (int): The number of elements from input_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    printed_count = 0
+    for i in range(count):
+        try:
+            print("{}".format(input_list[i]), end="")
+            printed_count += 1
+        except IndexError:
+            break
+    print("")
+    return printed_count
+
