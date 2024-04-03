a407b6fcd6fa3897a00bf07a33d958ff85a954c0
root <you@example.com>
2024-04-02 17:19:51 -0700

    d

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(slf, sz):
+        """Initialize a new Square.
+
+        Args:
+            sz (int): The sz of the new square.
+        """
+        slf.__sz = sz
